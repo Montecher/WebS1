b74b3148e63fcbe922fdc32f43d658ad8cfa25b6
Nathan DECHER <nathan.decher@gmail.com>
2018-10-19 00:06:31 +0200

    transposed everything to templates

--- build/template.lua
+++ build/template.lua
@@ -1,28 +1,72 @@
 #!/usr/bin/env lua
 
+-- load XML lib
 local xml=require 'luaxml.xml'
 
---BEGIN variables
-local indir='src/pages'
-local outdir='out'
---END variables
+-- load config
+local list=require 'src.templates'
 
 -- create output directory
 os.execute('mkdir -p out')
 
--- list all pages
-local pages={}
-for page in io.popen('ls -1 "'..indir..'"'):lines() do
-	table.insert(pages, page)
+-- file reader
+local function readall(filename)
+	local fd=io.open(filename, 'r')
+	local data=fd:read('*a')
+	fd:close()
+	return data
 end
-io.write(#pages.." pages found\n")
 
--- convert the page
-for i, page in ipairs(pages) do
-	io.write(page..'\n')
-	--FIXME use actual templates
-	os.execute('cp "'..indir..'/'..page..'" "'..outdir..'"')
+-- file writer
+local function writeall(filename, data)
+	local fd=io.open(filename, 'w')
+	fd:write(data)
+	fd:close()
+end
+
+-- template reader
+local function readtemplate(name)
+	return xml.parse(readall('src/templates/'..list.templates[name]))
+end
+
+-- indexof
+local function indexof(tab, val)
+	for i, v in ipairs(tab) do
+		if v==val then
+			return i
+		end
+	end
+end
+
+-- template renderer
+local function rendertemplate(template, values)
+	for k, v in pairs(values) do
+		local substituables=template:queryselectorall('substituable.'..k)
+		for i, substituable in ipairs(substituables) do
+			local siblings=substituable.parent.children
+			local index=indexof(siblings, substituable)
+			siblings[index]=xml.createtextnode(v)
+		end
+	end
+end
+
+-- render pages
+for k, v in pairs(list.pages) do
+	io.write("Rendering "..k..'\n')
+	
+	-- load the template
+	local template=readtemplate(v.template)
+	
+	-- load the data
+	v.values.html=readall('src/pages/'..k)
+	
+	-- render everything
+	rendertemplate(template, v.values)
+	
+	-- write the file
+	writeall('out/'..k, template:dump(true, true))
 end
 
 -- create the zip
+io.write("Writing zip\n")
 os.execute('zip -r pages.zip out')
---
+++ src/templates.lua
@@ -0,0 +1,48 @@
+local templates={}
+
+templates.base="base.html"
+templates.article="article.html"
+
+local pages={}
+
+pages["a_propos.html"]={}
+pages["a_propos.html"].template='base'
+pages["a_propos.html"].values={}
+pages["a_propos.html"].values.title="À propos"
+
+pages["article.html"]={}
+pages["article.html"].template='article'
+pages["article.html"].values={}
+pages["article.html"].values.title="Fregona"
+
+pages["article2.html"]={}
+pages["article2.html"].template='article'
+pages["article2.html"].values={}
+pages["article2.html"].values.title="Lama"
+
+pages["index.html"]={}
+pages["index.html"].template='base'
+pages["index.html"].values={}
+pages["index.html"].values.title="Accueil"
+
+pages["ma_commande.html"]={}
+pages["ma_commande.html"].template='base'
+pages["ma_commande.html"].values={}
+pages["ma_commande.html"].values.title="Ma commande"
+
+pages["ma_recherche.html"]={}
+pages["ma_recherche.html"].template='base'
+pages["ma_recherche.html"].values={}
+pages["ma_recherche.html"].values.title="Ma recherche"
+
+pages["nous_contacter.html"]={}
+pages["nous_contacter.html"].template='base'
+pages["nous_contacter.html"].values={}
+pages["nous_contacter.html"].values.title="Nous contacter"
+
+pages["panier.html"]={}
+pages["panier.html"].template='base'
+pages["panier.html"].values={}
+pages["panier.html"].values.title="Mon panier"
+
+return {templates=templates, pages=pages}
