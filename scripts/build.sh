c2abd2bfdc44b2e47f3c97567efa6221a50987ba
Nathan DECHER <nathan.decher@gmail.com>
2018-10-19 15:18:14 +0200

    refactoring in progress

---
+++ scripts/build.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/natnat-mc/luaxml
+build/assets.sh && build/template.lua && build/css.sh
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const express=require('express');
 const app=express();
-app.use(express.static('..'));
+app.use(express.static('out'));
 app.listen(12345);
 console.log('http://127.0.0.1:12345');
