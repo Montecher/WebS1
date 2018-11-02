14cb80b7bd8ce5015803c0adfcad4e3087d98db6
Nathan DECHER <nathan.decher@gmail.com>
2018-11-02 16:11:03 +0100

    added second article

--- src/templates.lua
+++ src/templates.lua
@@ -23,5 +23,28 @@ return {
 				},
 			}
 		}
+	},
+	["article2.html"]={
+		template="article.html",
+		values={
+			title="Lamas",
+			price=6942,
+			illustration={
+				{
+					url="assets/Lama_(1).png",
+					alt="Lama des montagnes",
+					id="i1",
+					next="i2"
+				},
+				{
+					url="assets/Best_boi.jpg",
+					alt="Best boi Lama",
+					id="i2",
+					prev="i1"
+				},
+			},
+			specs=[[Magnifique assortiment de Lama.
+Vous pouvez voir dans la galerie nos modèles d'exposition. En partenaria avec <a href="http://www.lamashopper.cl" target="_blank">Lama Shopper</a> nous pouvons vous proposer de choisir dès petit votre futur lama et de le voir grandir jusqu'à ce qu'il ai l'âge de voyager jusqu'à vous. Vous pouvez voir nos autres lamas disponibles sur envoi de la demande à notre addresse mail ou postale. Livraison par Fedex ou TNT possible sous 15 jours après réception de la commande si votre Lama à l'âge légal le permettant.]]
+		}
 	}
 }
